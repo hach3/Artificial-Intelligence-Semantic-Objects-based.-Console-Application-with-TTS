ea8aeee01b61a4d8571185ca86ae2d0f3cbc8bca
hach3 <hach3@live.fr>
2017-01-29 23:12:01 +0100

    Adding TTS (text to speech)

--- AIv3.0/AI/ai.cpp
+++ AIv3.0/AI/ai.cpp
@@ -1,7 +1,9 @@
 #include "ai.h"
 #include <algorithm>
 #include <iostream>
-
+#include <ctime>
+#include <sapi.h>
+#include "../stdafx.h"
 AI::AI() {
 
 }
@@ -25,22 +27,22 @@ void AI::understandSentence(std::string usersentence)
 	std::cout << "Start analyzing sentence." << std::endl;
 	if (this->_pronouns.getInterrogativePronouns().isInterrogativeInSentence(usersentence, newSentence, wh) == true)
 	{
-		std::cout << "Found an interrogative pronoun" << std::endl;
+	//	std::cout << "Found an interrogative pronoun" << std::endl;
 		//Si on trouve be dans la nouvelle sentence
 		if (this->_verbs.getAuxiliaryVerbs().isBeInSentence(newSentence, verb) == true)
 		{
-			std::cout << "Found be" << std::endl;
+		//	std::cout << "Found be" << std::endl;
 			//Si on trouve un adjectif possessif
 			if (this->_adjectives.getPossessiveAdjectives().isPossessiveSentence(newSentence, possessiveAdjectif) == true)
 			{
-				std::cout << "Found a possessive adjective" << std::endl;
+			//	std::cout << "Found a possessive adjective" << std::endl;
 				//Si on trouve un nom
 				if (this->_nouns.getCommonNouns().isCommonInSentence(newSentence, noun) == true)
 				{
-					std::cout << "Found an common noun" << std::endl;
+					//std::cout << "Found an common noun" << std::endl;
 					isWH = true;
-					this->_sentenceToSay = wh + " " + verb + " " + possessiveAdjectif + " " + noun + "?";
-					this->saySomething();
+					//this->_sentenceToSay = wh + " " + verb + " " + possessiveAdjectif + " " + noun + "?";
+					//this->saySomething();
 				}
 			}
 				
@@ -58,6 +60,32 @@ void AI::understandSentence(std::string usersentence)
 void AI::saySomething()
 {
 	std::cout << this->_sentenceToSay << std::endl;
+	//Let's convert the string to say in wstring
+	std::wstring text(this->_sentenceToSay.begin(), this->_sentenceToSay.end());
+
+	ISpVoice * pVoice = NULL;
+
+	if (FAILED(::CoInitialize(NULL)))
+	{
+
+	}
+	else
+	{
+		HRESULT hr = CoCreateInstance(CLSID_SpVoice, NULL, CLSCTX_ALL, IID_ISpVoice, (void **)&pVoice);
+		if (SUCCEEDED(hr))
+
+		{
+
+			hr = pVoice->Speak(text.c_str(), 0, NULL);
+			pVoice->Release();
+			pVoice = NULL;
+		}
+
+		::CoUninitialize();
+
+	}
+
+
 	this->_sentenceToSay = "";
 }
 
