8df48d9c6e21ff6198578215ff9b755f5cf29a05
hach3 <hach3@live.fr>
2017-02-05 18:27:38 +0100

    Testing next_permutation

--- NaturalLanguageProcessingv0.1.0/Language/Phrase_Structuration.cpp
+++ NaturalLanguageProcessingv0.1.0/Language/Phrase_Structuration.cpp
@@ -46,23 +46,31 @@ void Phrase_Structuration::getStructure(vector<WORD_AND_TYPE> sentenceTypes)
 	/* TRYING NEXT_PERMUTATION TO EXTRACT ALL COMBINATIONS OF PHRASE STRUCTURE */
 
 	//So i've got sentenceTypes which is a vector of vector of int
+	
 
+
+
+	
 	for (int i = 0; i < sentenceTypes.size(); i++)
-	{		
-		do {
-			cout << "Next permutation " << endl;
-			for (int y = 0; y < sentenceTypes.at(i).types.size(); y++)
-			{	
-				cout << sentenceTypes.at(i).types.at(y) << " ";
+	{
+		cout << "Possibility " << endl;
+		do
+		{
+			for (int j = 0; j < sentenceTypes.at(i).types.size(); j++)
+			{
+				cout << sentenceTypes.at(i).types.at(j) << " ";
 			}
 			cout << endl;
-		} while (next_permutation(sentenceTypes.at(i).types.begin(), sentenceTypes.at(i).types.begin() + sentenceTypes.at(i).types.size()));		
+		} while (next_permutation(sentenceTypes.at(i).types.begin(), sentenceTypes.at(i).types.begin() + sentenceTypes.at(i).types.size()));
+
 	}
 
 
+	
 	/*
 	
-	this->printAll(sentenceTypes, tmpVector, tmp2Vector,phraseWordsTypesVector, 0, 0);
+	this->printAll(sentenceTypes, tmpVector, tmp2Vector,phraseWordsTypesVector, 0, 0, 0);
+
 	for (unsigned int i = 0; i < phraseWordsTypesVector.size(); i++)
 	{		
 		if (phraseWordsTypesVector.at(i).size() == sentenceTypes.size())
@@ -76,29 +84,29 @@ void Phrase_Structuration::getStructure(vector<WORD_AND_TYPE> sentenceTypes)
 		}
 			
 		
-	}
-	*/
+	}*/
+	
 }
 
-void Phrase_Structuration::printAll(const vector<WORD_AND_TYPE> &allVecs, vector<WORDS_TYPE> &tmpVector, vector<WORDS_TYPE> tmp2Vector, vector<vector<WORDS_TYPE>> &phraseWordsTypesVector, size_t vecIndex, int strSoFar)
+
+
+
+void Phrase_Structuration::printAll(const vector<WORD_AND_TYPE> &allVecs, vector<WORDS_TYPE> &tmpVector, vector<WORDS_TYPE> tmp2Vector, vector<vector<WORDS_TYPE>> &phraseWordsTypesVector, size_t vecIndex, int strSoFar, int nb)
 {
 	if (vecIndex >= allVecs.size())
-	{
-	//	cout << "FINISHED POSSIBILITY : ";
+	{	
 		phraseWordsTypesVector.push_back(tmpVector);
-		//phraseWordsTypesVector.push_back(tmp2Vector);
 		tmpVector.clear();
-		tmp2Vector.clear();
-	//	cout << strSoFar << endl;
+		
+		cout << strSoFar << endl;
 		return;
 	}
+	
 	for (size_t i = 0; i < allVecs[vecIndex].types.size(); i++)
 	{
-		//cout << "Vec : " << vecIndex << endl;		
 		tmpVector.push_back(allVecs[vecIndex].types[i]);
-		printAll(allVecs, tmpVector, tmp2Vector, phraseWordsTypesVector, vecIndex + 1, strSoFar + allVecs[vecIndex].types[i]);
+		printAll(allVecs, tmpVector, tmp2Vector, phraseWordsTypesVector, vecIndex + 1, strSoFar + allVecs[vecIndex].types[i], allVecs[vecIndex].types[i]);
 		
-	//	cout << "Vector : " << vecIndex << endl;
 	}
-	
+
 }
--- NaturalLanguageProcessingv0.1.0/Language/Phrase_Structuration.h
+++ NaturalLanguageProcessingv0.1.0/Language/Phrase_Structuration.h
@@ -15,7 +15,7 @@ public:
 	
 
 	void getStructure(vector<WORD_AND_TYPE> sentenceTypes);
-	void printAll(const vector<WORD_AND_TYPE> &allVecs, vector<WORDS_TYPE> &tmpVector, vector<WORDS_TYPE> tmp2Vector, vector<vector<WORDS_TYPE>> &phraseWordsTypesVector, size_t vecIndex, int strSoFar);
+	void printAll(const vector<WORD_AND_TYPE> &allVecs, vector<WORDS_TYPE> &tmpVector, vector<WORDS_TYPE> tmp2Vector, vector<vector<WORDS_TYPE>> &phraseWordsTypesVector, size_t vecIndex, int strSoFar, int nb);
 	
 protected:
 private:
