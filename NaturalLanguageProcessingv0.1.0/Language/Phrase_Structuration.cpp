250bb892d4b43d9982ccd835164bb6b50766b9ef
hach3 <hach3@live.fr>
2017-02-05 16:12:18 +0100

    Updating structure combinations : Phrase_Structuration.cpp

--- NaturalLanguageProcessingv0.1.0/Language/Phrase_Structuration.cpp
+++ NaturalLanguageProcessingv0.1.0/Language/Phrase_Structuration.cpp
@@ -1,4 +1,6 @@
 #include "Phrase_Structuration.h"
+#include <algorithm>
+
 
 Phrase_Structuration::Phrase_Structuration()
 {
@@ -35,12 +37,32 @@ void Phrase_Structuration::getStructure(vector<WORD_AND_TYPE> sentenceTypes)
 {
 	vector<vector<WORDS_TYPE>> phraseWordsTypesVector;
 	vector<WORDS_TYPE> tmpVector;
+	vector<WORDS_TYPE> tmp2Vector;
 	string subject;
 	string aux;
 	string verb;
 	string complement;
+
+	/* TRYING NEXT_PERMUTATION TO EXTRACT ALL COMBINATIONS OF PHRASE STRUCTURE */
+
+	//So i've got sentenceTypes which is a vector of vector of int
+
+	for (int i = 0; i < sentenceTypes.size(); i++)
+	{		
+		do {
+			cout << "Next permutation " << endl;
+			for (int y = 0; y < sentenceTypes.at(i).types.size(); y++)
+			{	
+				cout << sentenceTypes.at(i).types.at(y) << " ";
+			}
+			cout << endl;
+		} while (next_permutation(sentenceTypes.at(i).types.begin(), sentenceTypes.at(i).types.begin() + sentenceTypes.at(i).types.size()));		
+	}
+
+
+	/*
 	
-	this->printAll(sentenceTypes, &phraseWordsTypesVector, tmpVector, 0, 0);	
+	this->printAll(sentenceTypes, tmpVector, tmp2Vector,phraseWordsTypesVector, 0, 0);
 	for (unsigned int i = 0; i < phraseWordsTypesVector.size(); i++)
 	{		
 		if (phraseWordsTypesVector.at(i).size() == sentenceTypes.size())
@@ -52,22 +74,31 @@ void Phrase_Structuration::getStructure(vector<WORD_AND_TYPE> sentenceTypes)
 			}
 			cout << endl;
 		}
+			
+		
 	}
+	*/
 }
 
-void Phrase_Structuration::printAll(const vector<WORD_AND_TYPE> &allVecs, vector<vector<WORDS_TYPE>>* phraseWordsTypesVector, vector<WORDS_TYPE> &tmpVector, size_t vecIndex, int strSoFar)
+void Phrase_Structuration::printAll(const vector<WORD_AND_TYPE> &allVecs, vector<WORDS_TYPE> &tmpVector, vector<WORDS_TYPE> tmp2Vector, vector<vector<WORDS_TYPE>> &phraseWordsTypesVector, size_t vecIndex, int strSoFar)
 {
-	
 	if (vecIndex >= allVecs.size())
-	{		
-		phraseWordsTypesVector->push_back(tmpVector);
+	{
+	//	cout << "FINISHED POSSIBILITY : ";
+		phraseWordsTypesVector.push_back(tmpVector);
+		//phraseWordsTypesVector.push_back(tmp2Vector);
 		tmpVector.clear();
-		cout << strSoFar << endl;
+		tmp2Vector.clear();
+	//	cout << strSoFar << endl;
 		return;
-	}	
+	}
 	for (size_t i = 0; i < allVecs[vecIndex].types.size(); i++)
-	{		
-		tmpVector.push_back(allVecs[vecIndex].types.at(i));
-		printAll(allVecs, phraseWordsTypesVector, tmpVector, vecIndex + 1, strSoFar + allVecs[vecIndex].types.at(i));			
-	}	
+	{
+		//cout << "Vec : " << vecIndex << endl;		
+		tmpVector.push_back(allVecs[vecIndex].types[i]);
+		printAll(allVecs, tmpVector, tmp2Vector, phraseWordsTypesVector, vecIndex + 1, strSoFar + allVecs[vecIndex].types[i]);
+		
+	//	cout << "Vector : " << vecIndex << endl;
+	}
+	
 }
